039640717049be2b52d88656a96862daa6b0e13d
Regonold <regonold520@gmail.com>
2026-08-09 04:40:49 +0100

    Created initial pixel looping and creation, implemented basic noise values

--- conf.lua
+++ conf.lua
@@ -1,5 +1,3 @@
 function love.conf(t)
-    t.window.width = 1869
-    t.window.height = 948
     t.window.resizable = true
 end
--- main.lua
+++ main.lua
@@ -1,100 +1,42 @@
-scalar = 10
+local renderable = nil
+local drawImg = nil
 
-Ui = require("ui")
-PixelService = require("pixelservice")
-Gui = require("gui")
-Tabs = require("tabs")
+local patternDim = {x=100, y = 100}
 
 function love.load()
-    love.mouse.setVisible( false )
     love.graphics.setDefaultFilter("nearest", "nearest")
-    love.graphics.setBackgroundColor(0.078,0.070,0.090)
-
-    PixelService:load()
-    Ui:load()
-    Gui:load()
-    Tabs:load()
-
-    newSpriteButton = registerButton("darkbutton.png", "dark", "New Sprite")
-
-    newSpriteButton.useFunc = function()
-        print("hi")
-        Ui:generateNewCanvas()
-        print("hi")
-    end
-
-    newSpriteButton.sizeX = 26
-
-    newSpriteButton.pos = {x=100,y=100}
-
-    newSpriteButton.bound = false
-end
-
-
-deltaTimer = 0
-function love.update(dt)
-    if PixelService.selectedArea ~= nil then
-        print(PixelService.selectedArea.startPos.x)
-    end
-
-    deltaTimer = deltaTimer + dt
-    PixelService:update(dt)
-    Ui:update(dt)
-    Gui:update(dt)
-    Tabs:update(dt)
-
-    mousejustpressed = false
-    mousejustreleased = false
-    justkeypressed = ""
-end
-
-
-function love.mousemoved( x, y, dx, dy, istouch )
-    Ui:mousemoved(x,y,dx,dy,istouch)
-end
-
-function love.draw()
-    love.graphics.setColor(0.145, 0.137, 0.156)
-    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-    love.graphics.setColor(1,1,1)
-
-    if Tabs.currentTab.type == "pixel" then
-        love.graphics.setColor(0.078,0.070,0.090)
-        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-    end
-    if Tabs.currentTab.type == "pixel" then
-        PixelService:draw()
-    end
     
-    Ui:draw()
-    Gui:draw()
-    Tabs:draw()
-
-    local x, y = love.mouse.getPosition()
-    if PixelService.tooLight and not(Ui.overUI) and Ui.objectHovering == nil then love.graphics.setColor(0, 0, 0) end
-    love.graphics.draw(Ui.currentIco, x, y, 0, 2, 2, Ui.currentIco:getWidth()/2, Ui.currentIco:getHeight() / 2)
+    renderable = love.image.newImageData(patternDim.x, patternDim.y)
+    
+    for x = 0, renderable:getWidth()-1 do
+        for y = 0, renderable:getHeight()-1 do
+            r, g, b, a = pixelLoop(x, y)
+            renderable:setPixel(x, y, r, g, b, a)
+        end
+    end
 end
 
-function clamp(min, val, max)
-    return math.max(min, math.min(val, max));
+function pixelLoop(x, y)
+    local amplitude = 10
+
+    local noise = love.math.noise(x/amplitude, y/amplitude, (x+y)/amplitude)
+    local noise2 = love.math.noise(y/amplitude, x/amplitude, (x+y)/amplitude)
+
+
+    local on = 1
+    if math.sin((noise*noise2)*10) > 0.5 then
+        on = 0
+    end
+
+    return 1,0,1,on
 end
 
-function love.wheelmoved(x, y)
-    Ui:wheelmoved(x, y)
+function love.update(dt)
+    drawImg = love.graphics.newImage(renderable)
 end
 
-function love.mousepressed(x, y, button, istouch)
-    Ui:mousepressed(x, y, button, istouch)
-end
-
-function love.mousereleased(x, y, button)
-   Ui:mousereleased(x,y,button)
-end
-
-function love.keypressed( key, scancode, isrepeat )
-    Ui:keypressed( key, scancode, isrepeat )
-end
-
-function love.keyreleased( key, scancode, isrepeat )
-   Ui:keyreleased( key, scancode, isrepeat )
+function love.draw(dt)
+    if drawImg ~= nil then
+        love.graphics.draw(drawImg, 20, 20, 0, 3, 3)
+    end
 end
